4e77f8059aaa6840cb529f6f5fb3d16ff10b6d20
Matthew Chen <mchen@macrofab.com>
2024-12-02 16:39:33 -0600

    refactors part 1 to move functionality into a function

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -1,25 +1,14 @@
 # Part 1
-safe_count = 0
-
-file = open('day_2/day_2_input.txt', 'r')
-
-for line in file:
-    report = line.strip().split()
-    report_int : list[int] = []
-    for level in report:  # convert all string to int
-        report_int.append(int(level))
-
-    safe = True
+def check_if_safe(report):
     all_increasing = True
     all_decreasing = True
     previous_level = None
-    for level in report_int:
+    for level in report:
         if previous_level == None:  # First element handling
             previous_level = level
             continue
         elif abs(level - previous_level) < 1 or abs(level - previous_level) > 3:  # Check difference conditions
-            safe = False
-            break
+            return False
 
         elif level > previous_level:
             all_decreasing = False
@@ -27,11 +16,22 @@ for line in file:
             all_increasing = False
         
         if not (all_increasing or all_decreasing):
-            safe = False
-            break
+            return False
         
         previous_level = level
-    if safe:
+    return True
+
+safe_count = 0
+    
+file = open('day_2/day_2_input.txt', 'r')
+
+for line in file:
+    report = line.strip().split()
+    report_int : list[int] = []
+    for level in report:  # convert all string to int
+        report_int.append(int(level))
+    
+    if check_if_safe(report_int):
         safe_count = safe_count + 1
 
 print(safe_count)
